621140833914b9cb404833290b86ded91c2c4af0
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-02-23 22:35:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,33 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	if (value == 2 || value == 3) return true;
+  if ((value * value) % 24 == 1) return true;
+  else return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int count = 0;
+  int i = 1;
+  while (count != n) {
+		i++;
+    if (checkPrime(i)) count++;
+  }
+	return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int i = value + 1;
+  while (!checkPrime(i)) i++;
+  return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+  int i = 2;
+  while (i != hbound) {
+		if (checkPrime(i)) sum += i;
+    i++;
+  }
+	return sum;
 }
