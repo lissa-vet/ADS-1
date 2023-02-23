9d8f359402184623453bd0037b1d12830fcfd186
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-02-24 00:00:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    int count = 0;
+    uint64_t count = 0;
     int i = 1;
     while (count != n) {
 	i++;
@@ -21,14 +21,14 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int i = value + 1;
+  uint64_t i = value + 1;
   while (!checkPrime(i)) i++;
   return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-  int i = 2;
+  uint64_t sum = 0;
+  uint64_t i = 2;
   while (i != hbound) {
 	if (checkPrime(i)) sum += i;
   	i++;
